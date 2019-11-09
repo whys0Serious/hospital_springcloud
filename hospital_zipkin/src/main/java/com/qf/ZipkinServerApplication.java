c270dd7c76c5ff9bc8d7095f89adbf61bba19ebf
86156 <547303896@qq.com>
2019-11-09 17:32:17 +0800

    纯净框架，第一次上传

---
+++ hospital_zipkin/src/main/java/com/qf/ZipkinServerApplication.java
@@ -0,0 +1,15 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableDiscoveryClient
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class);
+    }
+}
