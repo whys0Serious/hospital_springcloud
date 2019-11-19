93343e3db896d91d1ae7a9a1d9b38b7938f4e57e
zqzhuzhuxia <13772692011@163.com>
2019-11-19 14:21:10 +0800

    维修111发个

--- hospital_RabbitMQ_server/src/main/java/com/qf/config/RabbitMailListener.java
+++ hospital_RabbitMQ_server/src/main/java/com/qf/config/RabbitMailListener.java
@@ -1,6 +1,7 @@
 
 package com.qf.config;
 
+import com.qf.domain.UserMsg;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
